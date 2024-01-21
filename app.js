8081ffb6f3d8311fa75244e355b46a06d4b7f9ec
CodingWithError <vshravanth2004@gmail.com>
2024-01-21 22:20:57 +0530

    added some Html for buttons in js

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
-//selector
+// Selector
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
 
-//Event Listeners
-todoButton.addEventListener('click', addTodo());
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
 
-//Functions
-
-function addTodo(event){
+// Functions
+function addTodo(event) {
   event.preventDefault();
-  //to create a div in html.
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");//in this we added class to it.
-  //Now adding LI
-  const newToDo= document.createElement("li");
-  newToDo.classList("todo-item");
-  newToDo.innerText='hey';
+
+  // Create a div in HTML
+  const todoDiv = document.createElement('div');
+  todoDiv.classList.add('todo');
+
+  // Now adding LI
+  const newToDo = document.createElement('li');
+  newToDo.classList.add('todo-item');
+  newToDo.innerText = 'hey';
   todoDiv.appendChild(newToDo);
+
+  // Check Mark button
+  const completedButton = document.createElement('button');
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add('complete-btn');
+  todoDiv.appendChild(completedButton);
+
+  // Trash button
+  const trashButton = document.createElement('button');
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add('trash-btn');
+  todoDiv.appendChild(trashButton);
+
+  // Append to List
+  todoList.appendChild(todoDiv);
 }
